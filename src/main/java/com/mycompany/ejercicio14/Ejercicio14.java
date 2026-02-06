05c9fa562cfacc1ac6ca85381790c67ad6733b5b
Acer <Acer@DESKTOP-90MQKSB>
2026-02-06 16:15:05 -0600

    Refactor ejercicio 14 a programación orientada a objetos

--- src/main/java/com/mycompany/ejercicio14/Ejercicio14.java
+++ src/main/java/com/mycompany/ejercicio14/Ejercicio14.java
@@ -2,23 +2,35 @@ package com.mycompany.ejercicio14;
 
 import java.util.Scanner;
 
+class Numero {
+
+    private double valor;
+
+    public Numero(double valor) {
+        this.valor = valor;
+    }
+
+    public double obtenerCuadrado() {
+        return valor * valor;
+    }
+
+    public double obtenerCubo() {
+        return valor * valor * valor;
+    }
+}
+
 public class Ejercicio14 {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        double numero;
-        double cuadrado;
-        double cubo;
-
         System.out.print("Ingrese un número: ");
-        numero = sc.nextDouble();
+        double numeroIngresado = sc.nextDouble();
 
-        cuadrado = Math.pow(numero, 2);
-        cubo = Math.pow(numero, 3);
+        Numero num = new Numero(numeroIngresado);
 
-        System.out.println("El cuadrado es: " + cuadrado);
-        System.out.println("El cubo es: " + cubo);
+        System.out.println("El cuadrado del número es: " + num.obtenerCuadrado());
+        System.out.println("El cubo del número es: " + num.obtenerCubo());
     }
 }
